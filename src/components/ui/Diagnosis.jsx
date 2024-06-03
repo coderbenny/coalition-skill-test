2f450d0a066797e16d9fb401d87853270a36ac93
benny <benny.mathew@student.moringaschool.com>
2024-06-03 18:22:11 +0300

    added the rest of the data that is needed by the insight component

--- src/components/ui/Diagnosis.jsx
+++ src/components/ui/Diagnosis.jsx
@@ -8,9 +8,24 @@ export default function Diagnosis() {
       <div className="">
         <div className="bg-gray-200 h-[298px] w-full"></div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mt-3">
-          <Insight />
-          <Insight />
-          <Insight />
+          <Insight
+            title="Respiratory Rate"
+            metrics="20bpm"
+            comment="Normal"
+            icon="/respiratory_rate.png"
+          />
+          <Insight
+            title="Temperature'"
+            metrics="98.6F"
+            comment="Normal"
+            icon="/temperature.png"
+          />
+          <Insight
+            title="Heart Rate"
+            metrics="78bpm"
+            comment="Lower than average"
+            icon="/HeartBPM.png"
+          />
         </div>
         <DiagnosticList />
       </div>
--- src/components/ui/Insight.jsx
+++ src/components/ui/Insight.jsx
@@ -1,14 +1,10 @@
-export default function Insight() {
+export default function Insight({ icon, title, metrics, comment }) {
   return (
     <div className="h-[200px] p-3 w-full bg-gray-300 rounded-md">
-      <img
-        src="/respiratory_rate.png"
-        alt="diagnostics"
-        className="h-20 w-20 mb-2"
-      />
-      <h4 className="font-bold text-gray-600">Respiratory Rate</h4>
-      <p className="font-bold text-xl mb-3">20bpm</p>
-      <p className="font-bold text-sm tracking-wide">Normal</p>
+      <img src={icon} alt="diagnostics" className="h-20 w-20 mb-2" />
+      <h4 className="font-bold text-gray-600">{title}</h4>
+      <p className="font-bold text-xl mb-3">{metrics}</p>
+      <p className="font-bold text-sm tracking-wide">{comment}</p>
     </div>
   );
 }
